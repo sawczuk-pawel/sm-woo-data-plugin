3786c768998665432cffe6a0e4d1e7c3fce02dd6
Paweł Sawczuk <sawczuk.pawel@gmail.com>
2022-07-09 10:48:27 +0200

    Some clean in main plugin class

--- classes/SmMain.php
+++ classes/SmMain.php
@@ -1,47 +1,36 @@
 <?php
-
 namespace Sm;
+defined( 'ABSPATH' ) || exit;
+
 /**
  * Class SmMain
  * @package Sm
  */
 class SmMain {
     /**
-     * @var string
-     */
-    private static $requestUrl = 'https://httpbin.org/post';
-    /**
-     * @var string
-     */
-    private static $requestLogin = 'login_example'; // if autorized based login/pass
-    /**
-     * @var string
-     */
-    private static $requestPassword = 'pass_example'; // if autorized based login/pass
-    /**
-     * @var string
-     */
-    private static $requestKey = 'XSCSASA242880JFEIJS9SF9SF_EXAMPLE';
-    /**
-     * @var int
-     */
-    private static $loginInUserId;
-    /**
-     * @var string
-     */
-    private static $loginInUserMetaName = 'test_api_resonse';
-    /**
      * SmMain constructor.
      */
     public function __construct(){
+        add_action( 'init', array($this, 'init'));
+        add_action( 'widgets_init', array($this, 'initWidget') );
+    }
+
+    /**
+     * Initialize loading plugin
+     */
+    public function init(){
         if($this->checkWoocmmerceStatus()){
-            $this::$loginInUserId = get_current_user_id();
+            SmUser::setUserId();
             add_action('wp_enqueue_scripts', array($this, 'loadAssets'));
-            $init = new SmPanel();
+            new SmPanel();
         }
         else{
             add_action( 'admin_notices', array($this, 'showErrorNotice') );
         }
+    }
+
+    public function initWidget(){
+        register_widget( 'Sm\SmWidget' );
     }
 
     /**
@@ -50,7 +39,7 @@ class SmMain {
     public function showErrorNotice(){
         echo '<div class="notice notice-error is-dismissible">
       <p><strong>SM WooCommerce Data</strong> ' .
-      __('plugin for correct work needs active WooCommerce!') . '</p>
+      __('plugin for correct work needs active WooCommerce!', SM_TEXTDOMAIN) . '</p>
       </div>';
     }
 
@@ -70,6 +59,6 @@ class SmMain {
      * Load required assets
      */
     public function loadAssets(){
-        wp_enqueue_script('sm_custom_script', plugin_dir_url('/assets/script.js'), array(), '1', true);
+        wp_enqueue_script('sm_custom_script', plugin_dir_url( __FILE__ ) . '/../../assets/scripts.js', array(), SM_VERSION, true);
     }
 }
